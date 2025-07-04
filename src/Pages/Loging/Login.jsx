210a35c0f00af5ee5d5c6d3e4e46f0db49b7b411
kevin <kevin2013118@gmail.com>
2025-07-04 11:47:29 -0500

    Se mejora el auth1.7

--- src/Pages/Loging/Login.jsx
+++ src/Pages/Loging/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Layout from "../../Components/layout";
 import Footer from "../../Components/Footer/footer";
 import { useAuth } from "../../Components/AuthContext/AuthContext.jsx";
 import "./Loging.css";
@@ -8,7 +9,15 @@ export default function Login() {
   const navigate = useNavigate();
   const { login } = useAuth();
 
+  const [registrarData, setRegistrarData] = useState({
+    nombre: "",
+    correo: "",
+    contraseña: "",
+    rol: "Administrador",
+  });
+
   const [currentForm, setCurrentForm] = useState("loging");
+
   const [showPassword, setShowPassword] = useState(false);
 
   const changeForm = (formName) => {
@@ -19,7 +28,6 @@ export default function Login() {
   const handleClick = () => {
     window.scrollTo(0, 0);
   };
-
   // Iniciar sesión
   const handleIniciar = async (e) => {
     e.preventDefault();
@@ -45,19 +53,20 @@ export default function Login() {
         alert("Inicio de sesión exitoso");
         localStorage.setItem("user", JSON.stringify(data.usuario));
         login();
+
         navigate("/CrudNoticias");
       } else {
         alert(data.message || "Credenciales inválidas");
-        localStorage.setItem("isLoggedIn", "false");
+        localStorage.setItem("isLoging", "false");
       }
     } catch {
-      console.error("Error al iniciar sesión");
+      console.log("Error al iniciar sesión");
       alert("Error al iniciar sesión");
-      localStorage.setItem("isLoggedIn", "false");
+      localStorage.setItem("isLogin", "false");
     }
   };
 
-  // Recuperar contraseña
+  // Actualizar contraseña
   const handleActualizar = async (e) => {
     e.preventDefault();
 
@@ -65,6 +74,7 @@ export default function Login() {
     const pinSeguridad = e.target.forgetPassword.value;
     const nuevaContraseña = e.target.newPassword.value;
 
+    // Validaciones
     if (!correo || !pinSeguridad || !nuevaContraseña) {
       alert("Todos los campos son obligatorios");
       return;
@@ -76,7 +86,7 @@ export default function Login() {
     }
 
     if (!/^\d{4}$/.test(pinSeguridad)) {
-      alert("El PIN debe tener exactamente 4 dígitos");
+      alert("El PIN debe ser de exactamente 4 dígitos");
       return;
     }
 
@@ -97,7 +107,7 @@ export default function Login() {
 
       if (res.ok) {
         alert("¡Contraseña actualizada correctamente!");
-        changeForm("loging");
+        changeForm("loging"); // Volver al formulario de login
       } else {
         alert(data.message || "Error al actualizar la contraseña");
       }
@@ -108,130 +118,138 @@ export default function Login() {
   };
 
   return (
-    <div id="main-container">
-      <main className="formulararioPrincipal">
-        {/* Login */}
-        {currentForm === "loging" && (
-          <div className="Menu-Loging">
-            <form className="Texto" onSubmit={handleIniciar}>
-              <h2>Ingreso</h2>
-              <div className="input-box">
-                <label>Usuario</label>
-                <input
-                  type="text"
-                  className="field"
-                  placeholder="Correo"
-                  name="correo"
-                  required
-                />
-              </div>
-              <div className="input-box">
-                <label>Contraseña</label>
-                <input
-                  type={showPassword ? "text" : "password"}
-                  className="field"
-                  placeholder="Contraseña"
-                  name="contraseña"
-                  required
-                />
-                <div className="input-check">
-                  <h4>Mostrar contraseña</h4>
+    <>
+      <div id="main-container">
+        <Layout />
+        <main className="formulararioPrincipal">
+          {/* Login */}
+          {currentForm === "loging" && (
+            <div className="Menu-Loging">
+              <form className="Texto" onSubmit={handleIniciar}>
+                <h2>Ingreso</h2>
+                <div className="input-box">
+                  <label>Usuario</label>
                   <input
-                    type="checkbox"
-                    onChange={() => setShowPassword(!showPassword)}
+                    type="text"
+                    className="field"
+                    placeholder="Correo"
+                    name="correo"
+                    required
                   />
                 </div>
-              </div>
-              <div className="button-ingresar">
-                <button
-                  type="submit"
-                  className="ingresarButton"
-                  onClick={handleClick}
-                >
-                  Ingresar
-                </button>
-                <div className="usuarioNewRegister">
-                  <a
-                    href="#"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      changeForm("forgotPassword");
-                    }}
+                <div className="input-box">
+                  <label>Contraseña</label>
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    className="field"
+                    placeholder="Contraseña"
+                    name="contraseña"
+                    required
+                  />
+                  <div className="input-check">
+                    <h4>Mostrar contraseña</h4>
+                    <input
+                      type="checkbox"
+                      onChange={() => setShowPassword(!showPassword)}
+                    />
+                  </div>
+                </div>
+                <div className="button-ingresar">
+                  <button
+                    type="submit"
+                    className="ingresarButton"
+                    onClick={handleClick}
                   >
-                    Olvidé mi contraseña
-                  </a>
+                    Ingresar
+                  </button>
+                  <div className="usuarioNewRegister">
+                    <a
+                      href="#"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        changeForm("forgotPassword");
+                      }}
+                    >
+                      Olvidé mi contraseña
+                    </a>
+                  </div>
                 </div>
-              </div>
-            </form>
-          </div>
-        )}
+              </form>
+            </div>
+          )}
 
-        {/* Recuperar Contraseña */}
-        {currentForm === "forgotPassword" && (
-          <div className="Menu-Password">
-            <form className="Texto" onSubmit={handleActualizar}>
-              <h2>Recuperar Contraseña</h2>
-              <div className="input-box">
-                <label>Correo Electrónico</label>
-                <input
-                  type="email"
-                  className="field"
-                  placeholder="Ingrese su correo"
-                  name="correo"
-                  required
-                />
-              </div>
-              <div className="input-box">
-                <label>PIN de Seguridad (4 dígitos)</label>
-                <input
-                  type="password"
-                  className="field"
-                  placeholder="Ingrese su PIN"
-                  name="forgetPassword"
-                  pattern="\d{4}"
-                  maxLength={4}
-                  required
-                />
-              </div>
-              <div className="input-box">
-                <label>Nueva Contraseña</label>
-                <input
-                  type={showPassword ? "text" : "password"}
-                  className="field"
-                  placeholder="Mínimo 8 caracteres"
-                  name="newPassword"
-                  minLength={8}
-                  required
-                />
-                <div className="input-check">
-                  <span>Mostrar contraseña</span>
+          {/* Recuperar Contraseña */}
+          {currentForm === "forgotPassword" && (
+            <div className="Menu-Password">
+              <form className="Texto" onSubmit={handleActualizar}>
+                <h2>Recuperar Contraseña</h2>
+
+                <div className="input-box">
+                  <label>Correo Electrónico</label>
                   <input
-                    type="checkbox"
-                    onChange={() => setShowPassword(!showPassword)}
+                    type="email"
+                    className="field"
+                    placeholder="Ingrese su correo"
+                    name="correo"
+                    required
                   />
                 </div>
-              </div>
-              <div className="button-contraseña">
-                <button type="submit" className="registerButton">
-                  Actualizar Contraseña
-                </button>
-              </div>
-              <div className="VolverInicio">
-                <button
-                  type="button"
-                  className="back-button"
-                  onClick={() => changeForm("loging")}
-                >
-                  Volver al inicio de sesión
-                </button>
-              </div>
-            </form>
-          </div>
-        )}
-      </main>
-      <footer>
-        <Footer />
-      </footer>
-    </div>
+
+                <div className="input-box">
+                  <label>PIN de Seguridad (4 dígitos)</label>
+                  <input
+                    type="password"
+                    className="field"
+                    placeholder="Ingrese su PIN"
+                    name="forgetPassword"
+                    pattern="\d{4}"
+                    maxLength={4}
+                    required
+                  />
+                </div>
+
+                <div className="input-box">
+                  <label>Nueva Contraseña</label>
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    className="field"
+                    placeholder="Mínimo 8 caracteres"
+                    name="newPassword"
+                    minLength={8}
+                    required
+                  />
+                  <div className="input-check">
+                    <span>Mostrar contraseña</span>
+                    <input
+                      type="checkbox"
+                      onChange={() => setShowPassword(!showPassword)}
+                    />
+                  </div>
+                </div>
+
+                <div className="button-contraseña">
+                  <button type="submit" className="registerButton">
+                    Actualizar Contraseña
+                  </button>
+                </div>
+
+                <div className="VolverInicio">
+                  <button
+                    type="button"
+                    className="back-button"
+                    onClick={() => changeForm("loging")}
+                  >
+                    Volver al inicio de sesión
+                  </button>
+                </div>
+              </form>
+            </div>
+          )}
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    </>
   );
 }
