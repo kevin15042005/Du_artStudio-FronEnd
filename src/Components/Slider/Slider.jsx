0e1940d8b70eac2cc780ad2a7c40ab9b2de4522c
kevin <kevin2013118@gmail.com>
2025-07-04 23:35:41 -0500

    Slider arreglado en la app

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -25,7 +25,7 @@ function Slider() {
               src={`${import.meta.env.VITE_API_URL}/uploads/${
                 aliado.imagen_Marcas_Aliadas
               }`}
-  alt={aliado.nombre_Marcas_Aliadas}
+              alt={aliado.nombre_Marcas_Aliadas}
             />
             <p>{aliado.nombre_Marcas_Aliadas}</p>
           </div>
@@ -33,8 +33,6 @@ function Slider() {
       </div>
     </div>
   );
-  
-
 }
 
 export default Slider;
